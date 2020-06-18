308854fa69b759e3c27fbbb522334160632efc14
tobecci <41482784+tobecci@users.noreply.github.com>
2020-06-18 13:28:41 +0100

    FIX: Made the periods and days adjustable

---
+++ README.md
@@ -0,0 +1,6 @@
+#TIME TABLE GENERATOR
+
+## SETUP
+
+* clone the repo
+* in the project directory type `node index.js`
--- index.js
+++ index.js
@@ -1,34 +1,44 @@
 console.log("Generating time table");
 
 const lecturers = [
-    {name: "Mr A", subject: "maths"},
-    {name: "Mrs B", subject: "english"},
-    {name: "Mr C", subject: "crk"},
-    {name: "Mrs D", subject: "csc"},
-    {name: "Mr E", subject: "arts"},
-    {name: "Mrs F", subject: "business"},
-    {name: "Mr G", subject: "ens"},
-    {name: "Mrs F", subject: "physics"},
+    {name: "Dennis", subject: "maths"},
+    {name: "Jeffery", subject: "english"},
+    {name: "Egwu", subject: "crk"},
+    {name: "Julian", subject: "csc"},
+    {name: "Ada", subject: "arts"},
+    {name: "Zoe", subject: "business"},
+    {name: "Jane", subject: "ens"},
+    {name: "Olivia", subject: "physics"},
     {name: null, subject: null},
 ];
 
 //the null field signifies a free period
 
-// console.log(lecturers);
 
 //define a two  dimensional array
-let timetable = [
-    new Array(7),
-    new Array(7),
-    new Array(7),
-    new Array(7),
-    new Array(7),
-    new Array(7),
-    new Array(7),
-];
 
+const table = [];
+const periods = 4
+const days = 5;
+
+if(periods > 10){
+    console.log("maiximum number of periods is 10");
+    console.log("exiting");
+    process.exit();
+}
+
+for(let i = 0; i<days; i++){
+    table.push(new Array(periods));
+}
+
+/** to check if a lecture can be added to that day's instance
+ *
+ * @param day
+ * @param lecturer
+ * @param index
+ * @return {boolean}
+ */
 function canAdd(day, lecturer,index) {
-    // console.log({day: day, lecturer: lecturer, occurence: occurences(day, lecturer)});
     let previousSubject = JSON.stringify(day[index-1]);
     let currentSubject = JSON.stringify(lecturer);
     let isPreceding = previousSubject === currentSubject;
@@ -38,6 +48,12 @@ function canAdd(day, lecturer,index) {
     return canAdd;
 }
 
+/**checks how many times a lecture has been added to a days
+ *
+ * @param array
+ * @param item
+ * @return {number}
+ */
 function occurences(array, item) {
     let occurence = 0;
     for (let i = 0; i < array.length; i++) {
@@ -50,22 +66,20 @@ function occurences(array, item) {
     return occurence;
 }
 
-let stored = [];
-
-for (let i = 0; i < timetable.length; i++) {
-    for (let j = 0; j < timetable.length; j++) {
+//main logic for generating the time table
+for (let i = 0; i < days; i++) {
+    for (let j = 0; j < periods; j++) {
         let randomNumber = Math.floor(Math.random() * Math.floor(lecturers.length));
         let lecturer = lecturers[randomNumber]
         // while the lecture cannot be added reselect a lecture at random
-        while(!canAdd(timetable[i], lecturer, j)){
+        while(!canAdd(table[i], lecturer, j)){
             randomNumber = Math.floor(Math.random() * Math.floor(lecturers.length));
             lecturer = lecturers[randomNumber];
         }
-        timetable[i][j] = lecturer;
-        stored.push(lecturer);
+        table[i][j] = lecturer;
     }
 }
 
-console.log(timetable);
+console.log(table);
 
 
